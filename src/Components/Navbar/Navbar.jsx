12408baf97befb85e17a228316a12443a393b9f7
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-03-10 09:53:36 -0700

    Adding interacivity to our navbar icon.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/kaytechLogo.png";
 
 function Navbar() {
+  const [navState, setNavState] = useState('');
+  const [sticky, setSticky] = useState('');
+
+  useEffect(() =>{
+    window.addEventListener('scroll', ()=> {
+      setSticky(() => {
+        return window.scrollY > 50 ? true : false;
+      })
+    })
+  }, [])
+
+  const handleNavState = () => {
+    setNavState(!navState);
+  };
+
   return (
     <div>
-      <nav className="nav-dark">
+      <nav className={`${sticky ? "nav-dark" : ""}`}>
         <div className="nav-brand">
           <img src={logo} alt="" />
         </div>
-        <ul className="nav-links open">
+        <ul className={`nav-links ${navState ? "open" : ""}`}>
           <li>Home</li>
           <li>Courses</li>
           <li>Contact</li>
@@ -17,7 +32,10 @@ function Navbar() {
           <li>Gallery</li>
           <li>Testimonials</li>
         </ul>
-        <div className="menu-icon open">
+        <div
+          className={`menu-icon ${navState ? "open" : ""}`}
+          onClick={handleNavState}
+        >
           <div className="menu-icons">
             <span className="icon"></span>
           </div>
